f5e734dceacaeca493444a3ee84e09cb99f570d3
Lucaalex00 <lu.al.cirio00@gmail.com>
2024-02-16 14:46:12 +0100

    Add Esercizio 3 Commit

--- assets/js/Script.js
+++ assets/js/Script.js
@@ -20,4 +20,21 @@ let radius = 8;
 let circle = (radius * 2) * PI
 console.log(circle)
 
+//ESERCIZIO 2C ---> String Value
+
+let name = "Carlo"
+name = "Marco"
+console.log(name)
+
+// IN QUESTA SCHERMATA "NAME" Rinominata la seconda volta risulta deprecata
+// Causa variabile globale con speciali utilizzi.
+
+//SOLUZIONE BONUS
+
+!(function () {
+    let name = "Carlo"
+    name = "Marco"
+    name = "Fabio Pacifici"
+    console.log(name)
+})()
 
