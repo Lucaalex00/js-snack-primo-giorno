fdb6d428db51aa2b94222925e3bddb7aca0cd6ce
Lucaalex00 <lu.al.cirio00@gmail.com>
2024-02-16 14:41:20 +0100

    Add Esercizio 2B/2C Commit

--- assets/js/Script.js
+++ assets/js/Script.js
@@ -8,3 +8,16 @@ console.log(number);
 const PI = 3.14;
 console.log(PI);
 
+//ESERCIZIO 2B ---> ERROR Value
+/*
+PI = 3.1416                                     
+console.log(PI)
+*/
+
+//ESERCIZIO 2C ---> Number Value
+
+let radius = 8;
+let circle = (radius * 2) * PI
+console.log(circle)
+
+
